bd945d42264e3f408f3be9bdd2333f364ea22be4
James Cox <james@imaj.es>
2018-03-27 14:02:35 -0400

    Tentative fix here to resolve where product images are missing

--- core/app/helpers/spree/base_helper.rb
+++ core/app/helpers/spree/base_helper.rb
@@ -192,10 +192,10 @@ module Spree
         options = options.first || {}
         if product.images.empty?
           if !product.is_a?(Spree::Variant) && !product.variant_images.empty?
-            create_product_image_tag(product.variant_images.first, product, options, style)
+            create_product_image_tag(product.front_images.first, product, options, style)
           else
             if product.is_a?(Variant) && !product.product.variant_images.empty?
-              create_product_image_tag(product.product.variant_images.first, product, options, style)
+              create_product_image_tag(product.product.front_images.first, product, options, style)
             else
               image_tag "noimage/#{style}.png", options
             end
